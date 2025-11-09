33409da7bda14cac80fe958a3caa0d834c794523
Austin Asher <nintendoemailthing@yahoo.com>
2025-11-09 10:40:16 -0800

    added unmute button

--- src/app/unmute.tsx
+++ src/app/unmute.tsx
@@ -2,4 +2,15 @@
 
 import { useState } from "react";
 
-export default function unmute() {}
+export default function Unmute() {
+  const [muted, setMuted] = useState(true);
+
+  return (
+    <button
+      onClick={() => setMuted(false)}
+      className={muted ? "text-red-500" : "text-green-500"}
+    >
+      {muted ? "muted" : "unmuted"}
+    </button>
+  );
+}
